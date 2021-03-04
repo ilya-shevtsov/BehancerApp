cbdef35680e17556c4f00d642b48b53c19a3c3c6
ilya-shevtsov <Dayzilya@gmail.com>
2021-03-04 17:04:10 +0300

    init repository:

---
+++ app/src/main/java/com/elegion/test/behancer/AppDelegate.java
@@ -0,0 +1,33 @@
+package com.elegion.test.behancer;
+
+import android.app.Application;
+
+
+import androidx.room.Room;
+
+import com.elegion.test.behancer.data.database.BehanceDatabase;
+import com.elegion.test.behancer.data.Storage;
+
+/**
+ * Created by Vladislav Falzan.
+ */
+
+public class AppDelegate extends Application {
+
+    private Storage mStorage;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        final BehanceDatabase database = Room.databaseBuilder(this, BehanceDatabase.class, "behance_database")
+                .fallbackToDestructiveMigration()
+                .build();
+
+        mStorage = new Storage(database.getBehanceDao());
+    }
+
+    public Storage getStorage() {
+        return mStorage;
+    }
+}
